ee6078b69d300916d7266f8b80c3a4f209b955af
Sujay HR <93090191+Sujayhr@users.noreply.github.com>
2022-01-17 15:23:12 +0000

    nothing

---
+++ p1original.c
@@ -0,0 +1,11 @@
+ #include<stdio.h>
+int main()
+{
+  int a,b,c;
+  printf("enter the value of a,b\n");
+  scanf("%d%d" ,&a,&b);
+  c=a+b;
+  printf("the sum of two numbers=%d\n",c);
+  retur0;
+
+}
