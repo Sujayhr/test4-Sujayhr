9f71010aa7c37790017e73080e9134aad4621d73
Sujay HR <93090191+Sujayhr@users.noreply.github.com>
2022-01-17 15:26:06 +0000

    nothing

---
+++ p2original.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int input()
+
+{ 
+  int a;
+  printf("enter the value\n");
+  scanf("%d",&a);
+  return a;
+} 
+int add(int a, int b) 
+{
+int sum;
+sum=a+b;
+return sum;
+}
+void output(int a, int b, int sum)
+{
+printf("sum of %d and %d is %d",a,b,sum);
+}
+int main()
+{
+  int x,y,sum;
+  x=input();
+  y=input();
+  sum=add(x,y);
+  output(x,y,sum);
+  return 0;
+}
